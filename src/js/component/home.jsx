c1c0e85f516a92fadff4c1680af89aa49d6c2d6e
dadiazr07 <dadiazr.07@gmail.com>
2023-02-04 19:43:16 +0000

    Ajuste UL Li

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,25 +8,33 @@ const Home = () => {
 	const [list, setList] = useState([])
 	const [counter, setCounter] = useState(0)
 	const [inputValue, setInputValue] = useState("")
+
 	const handleEnter = (enter) =>{
 		if(enter.key === 'Enter'){
-			setList([...list,inputValue]);
-			setInputValue("")
+			if(inputValue != ""){
+				setList([...list,inputValue]);
+				setCounter = 
+				setInputValue("")
+			}
 		}
 	}
 
 
 	return (
 		<>
-		<div className="text-center">
-			<input type='text' placeholder="Write your stupid task" onChange={(e)=> setInputValue(e.target.value)} value={inputValue}
-			onKeyDown={handleEnter}></input>
-			<ul className="list-group-numbered p-0">
-				{list.map((task, index) => {
-					return (<li key={index} className="list-group-item mt-2 mb-2 p-0">{task}</li>)
-				})}
-			</ul>
-		</div>
+			<div className="text-center">
+				<input type='text' placeholder="Write your stupid task" onChange={(e)=> setInputValue(e.target.value)} value={inputValue}
+				onKeyDown={handleEnter}></input>
+				<ul className="list-group-numbered p-0">
+					{list.map((task, index) => {
+						return (<div className="list-group-item d-flex justify-content-center row" key={index}>
+									<li className="col-4">{task}</li>
+									<span className="col-1">X</span>
+								</div>
+							)
+					})}
+				</ul>
+			</div>
 		</>
 	);
 };
