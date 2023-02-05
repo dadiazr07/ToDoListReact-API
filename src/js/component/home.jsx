013fc375aabd9a7612a4cd3270fe617c58854324
dadiazr07 <dadiazr.07@gmail.com>
2023-02-05 16:59:34 +0000

    List, show X onHover

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
 	const [list, setList] = useState([])
 	const [counter, setCounter] = useState(0)
 	const [inputValue, setInputValue] = useState("")
+	const [isHovering, setIsHovering] = useState(false);
 
 	const handleEnter = (enter) =>{
 		if(enter.key === 'Enter'){
@@ -19,21 +20,31 @@ const Home = () => {
 		}
 	}
 
+	const handleMouseOver = () => {
+	  setIsHovering(true);
+	};
+	const handleMouseOut = () => {
+	  setIsHovering(false);
+	};
+	// onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}
 
 	return (
 		<>
-			<div className="text-center">
-				<input type='text' placeholder="Write your stupid task" onChange={(e)=> setInputValue(e.target.value)} value={inputValue}
-				onKeyDown={handleEnter}></input>
-				<ul className="list-group-numbered p-0">
-					{list.map((task, index) => {
-						return (<div className="list-group-item d-flex justify-content-center row" key={index}>
-									<li className="col-4">{task}</li>
-									<span className="col-1">X</span>
-								</div>
-							)
-					})}
-				</ul>
+			<div className="container w-100 d-flex justify-content-center py-5">
+				<div className="row w-50">
+					<input className="col" type='text' placeholder="Write your stupid task" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={handleEnter}></input>
+					<ul className="list-group list-group-flush list-unstyled p-0 mb-0">
+						{list.map((task, index) => {
+							return (<div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="list-group-item d-flex justify-content-start row" key={index}>
+										<li className="col-11">{task}</li>
+										{isHovering && 
+										<span className="col-1 badge bg-danger rounded-pill ">X</span>
+										}
+									</div>
+									)})}
+					</ul>
+					<p>{list.length < 2 ? `${list.length} task left` :  `${list.length} tasks left`}</p>
+				</div>
 			</div>
 		</>
 	);
