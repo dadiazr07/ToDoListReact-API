e44e3dcbe95178ef737b070852aa050000932cef
dadiazr07 <dadiazr.07@gmail.com>
2023-02-05 17:38:47 +0000

    When no tasks, msg. Need function to delete one

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,6 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [list, setList] = useState([])
-	const [counter, setCounter] = useState(0)
 	const [inputValue, setInputValue] = useState("")
 	const [isHovering, setIsHovering] = useState(false);
 
@@ -30,20 +29,19 @@ const Home = () => {
 
 	return (
 		<>
-			<div className="container w-100 d-flex justify-content-center py-5">
-				<div className="row w-50">
-					<input className="col" type='text' placeholder="Write your stupid task" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={handleEnter}></input>
-					<ul className="list-group list-group-flush list-unstyled p-0 mb-0">
+			<div className="container w-75 d-flex justify-content-center py-5">
+				<div className="container row w-50 bg-light rounded-3">
+					<input className="col bg-light form-control" type='text' placeholder="Write your stupid task" onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={handleEnter}></input>
+					<ul className="list-group list-group-flush list-unstyled p-0 mb-0 bg-light">
+						{list.length<1? <p className="ms-3 mt-2 mb-1"><strong className="text-capitalize text-secondary">No tasks! Add a task, idiot...</strong></p>: null} 
 						{list.map((task, index) => {
-							return (<div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="list-group-item d-flex justify-content-start row" key={index}>
-										<li className="col-11">{task}</li>
-										{isHovering && 
-										<span className="col-1 badge bg-danger rounded-pill ">X</span>
-										}
+							return (<div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="list-group-item d-flex justify-content-start row bg-light" key={index}>
+										<li className="col-11 bg-light"><strong className="text-capitalize text-secondary">{task}</strong></li>
+										{isHovering && <span className="col-1 bg-light"><strong className="badge bg-danger rounded-pill ">X</strong></span>}
 									</div>
 									)})}
 					</ul>
-					<p>{list.length < 2 ? `${list.length} task left` :  `${list.length} tasks left`}</p>
+					<p className="text-capitalize text-secondary bg-light">{list.length < 2 ? `${list.length} task left` :  `${list.length} tasks left`}</p>
 				</div>
 			</div>
 		</>
