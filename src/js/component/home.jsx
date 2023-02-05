298e07940ebd9a983f7b4e67bf1384f3064df4ee
dadiazr07 <dadiazr.07@gmail.com>
2023-02-05 21:34:47 +0000

    onHover individual listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,43 +7,36 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [list, setList] = useState([])
 	const [inputValue, setInputValue] = useState("")
-	const [isHovering, setIsHovering] = useState(false);
+	const [hoveringIndex, setHoveringIndex] = useState(null);
 
 	const handleEnter = (enter) =>{
 		if(enter.key === 'Enter'){
 			if(inputValue != ""){
 				setList([...list,inputValue]);
-				setCounter = 
 				setInputValue("")
 			}
 		}
 	}
-
-	const handleMouseOver = () => {
-	  setIsHovering(true);
-	};
-	const handleMouseOut = () => {
-	  setIsHovering(false);
-	};
-	// onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}
 
 	return (
 		<>
 			<div className="container w-75 d-flex justify-content-center py-5">
 				<div className="container row w-50 bg-light rounded-3">
 					<input className="col bg-light form-control" type='text' placeholder="Write your stupid task." onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={handleEnter}></input>
+
 					<ul className="list-group list-group-flush list-unstyled p-0 mb-0 bg-light">
-						{list.length<1? <p className="ms-3 mt-2 mb-1"><strong className="text-capitalize text-secondary">No tasks! Add a task, idiot...</strong></p>: null} 
-						{list.map((task, index) => {
-							return (<div className="list-group-item d-flex justify-content-start row bg-light" key={index}>
-										{isHovering == true ? 
-										<><li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="col-11 bg-light"><strong className="text-capitalize text-secondary">{task}</strong></li>
+						{list.length < 1 ? <p className="ms-3 mt-2 mb-1"><strong className="text-capitalize text-secondary">No tasks! Add a task, idiot...</strong></p> : 
+							list.map((task, index) => {
+							return (<div onMouseOver={() => setHoveringIndex(index)} onMouseLeave={() => setHoveringIndex(null)} className="list-group-item d-flex justify-content-start row bg-light" key={index}>
+										{hoveringIndex === index ? 
+										<><li className="col-11 bg-light"><strong className="text-capitalize text-secondary">{task}</strong></li>
 										<span className="col-1 bg-light"><strong className="badge bg-danger rounded-pill ">X</strong></span></>
 										:
-										<li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="col-11 bg-light"><strong className="text-capitalize text-secondary">{task}</strong></li>}
+										<li className="col-11 bg-light"><strong className="text-capitalize text-secondary">{task}</strong></li>}
 									</div>
 									)})}
 					</ul>
+
 					<p className="text-capitalize text-secondary bg-light">{list.length < 2 ? (list.length > 0 ? `${list.length} stuff left to do...` : `Looks like you don't have a life...`) :  `${list.length} "interesting" tasks left...`}</p>
 				</div>
 			</div>
