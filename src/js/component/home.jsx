d3ef9c46a2cd90b99fba38d1546c3543ca7af30a
dadiazr07 <dadiazr.07@gmail.com>
2023-02-06 22:17:01 +0000

    Detalles esteticos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
 		<>
 			<div className="container w-75 d-flex justify-content-center py-5">
 				<div className="container row w-50 bg-light rounded-3">
-					<input className="col bg-light form-control" type='text' placeholder="Write your stupid task." onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={handleEnter}></input>
+					<input className="col bg-light form-control m-1" type='text' placeholder="Write your stupid task." onChange={(e)=> setInputValue(e.target.value)} value={inputValue} onKeyDown={handleEnter}></input>
 
 					<ul className="list-group list-group-flush list-unstyled p-0 mb-0 bg-light">
 						{list.length < 1 ? <p className="ms-3 mt-2 mb-1"><strong className="text-capitalize text-secondary">No tasks! Add a task, idiot...</strong></p> : 
