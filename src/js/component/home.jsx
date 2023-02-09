11305f53b56d54fe3363f4c5946a2f3225909c2b
dadiazr07 <dadiazr.07@gmail.com>
2023-02-09 00:41:11 +0000

    GET does not need BODY

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -23,6 +23,34 @@ const Home = () => {
 		setList(newList);
 	}
 
+
+	const fetchTry = () => {
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/restrepo', {
+      method: "GET",
+    //   body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // will be true if the response is successfull
+        console.log(resp.status); // the status code = 200 or code = 400 etc.
+        console.log(resp.text()); // will try return the exact result as string
+        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //here is were your code should start after the fetch finishes
+        console.log(data); //this will print on the console the exact object received from the server
+    })
+    .catch(error => {
+        //error handling
+        console.log(error);
+    });
+	}
+	fetchTry()
+	
+
+
 	return (
 		<>
 			<div className="container w-75 d-flex justify-content-center py-5">
