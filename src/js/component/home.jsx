0919d159cb10082e2eef185432f52b45724fc12d
dadiazr07 <dadiazr.07@gmail.com>
2023-02-10 22:16:06 +0000

    setList(data.map(obj => obj.label))

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,3 +1,4 @@
+import { element, object } from "prop-types";
 import React, { useEffect, useState } from "react";
 
 //include images into your bundle
@@ -24,30 +25,18 @@ const Home = () => {
 	}
 
 
-	const fetchTry = () => {
+	useEffect(() => {
 		fetch('https://assets.breatheco.de/apis/fake/todos/user/restrepo', {
-      method: "GET",
-    //   body: JSON.stringify(todos),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // will be true if the response is successfull
-        console.log(resp.status); // the status code = 200 or code = 400 etc.
-        console.log(resp.text()); // will try return the exact result as string
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //here is were your code should start after the fetch finishes
-        console.log(data); //this will print on the console the exact object received from the server
-    })
-    .catch(error => {
-        //error handling
-        console.log(error);
-    });
-	}
-	fetchTry()
+			method: "GET",
+		  	headers: {
+		    	"Content-Type": "application/json"
+		  	}
+			})
+		  .then(resp => resp.json())
+		  .then(data => setList(data.map(object => object.label)))
+		  .catch(error => console.error(error));
+	  }, []);
+
 	
 
 
