6b68ae4b6853b5f50c62cfe8db31db2f9ebec8c9
dadiazr07 <dadiazr.07@gmail.com>
2023-02-11 01:32:57 +0000

    fetch put not done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,6 +10,10 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState("")
 	const [hoveringIndex, setHoveringIndex] = useState(null);
 
+
+	auxList = list.map(obj => {`label: ${obj}`, 'done: false'})
+	console.log(auxList)
+
 	const handleEnter = (enter) =>{
 		if(enter.key === 'Enter'){
 			if(inputValue != ""){
@@ -24,8 +28,7 @@ const Home = () => {
 		setList(newList);
 	}
 
-
-	useEffect(() => {
+	const getData = () => {
 		fetch('https://assets.breatheco.de/apis/fake/todos/user/restrepo', {
 			method: "GET",
 		  	headers: {
@@ -33,9 +36,49 @@ const Home = () => {
 		  	}
 			})
 		  .then(resp => resp.json())
-		  .then(data => setList(data.map(object => object.label)))
+		  .then(data => setList(data.map(({label}) => label)))
 		  .catch(error => console.error(error));
-	  }, []);
+	}
+
+	// const putData = () => {
+	// 	fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+	// 		method: "PUT",
+	// 		body: JSON.stringify(auxList),
+	// 		headers: {
+	// 			"Content-Type": "application/json"
+	// 		}
+	// 		})
+	// 		.then(resp => {
+	// 			console.log(resp.ok); // will be true if the response is successfull
+	// 			console.log(resp.status); // the status code = 200 or code = 400 etc.
+	// 			console.log(resp.text()); // will try return the exact result as string
+	// 			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+	// 		})
+	// 		.then(data => {
+	// 			//here is were your code should start after the fetch finishes
+	// 			console.log(data); //this will print on the console the exact object received from the server
+	// 		})
+	// 		.catch(error => {
+	// 			//error handling
+	// 			console.log(error);
+	// 		});
+	// }
+
+
+
+
+
+
+
+
+
+	useEffect(
+		() => getData()
+	
+
+
+		
+		, []);
 
 	
 
